228672ff95c3f6007e791a1daf14b2dfa44b13f4
machoverloard <138420269+machoverloard@users.noreply.github.com>
2023-10-13 10:41:30 +0530

    Add files via upload

--- gymhtml/scriptreg.js
+++ gymhtml/scriptreg.js
@@ -92,5 +92,20 @@ function updateDistrictOptions() {
 
 stateSelect.addEventListener("change", updateDistrictOptions);
 
-
 updateDistrictOptions();
+
+
+
+var todayDate = new Date();
+var month = todayDate.getMonth()+1;
+var year = todayDate.getUTCFullYear();
+var tdate = todayDate.getDate();
+if(month < 10){
+    month = "0" + month;
+}
+if(tdate < 10){
+    tdate = "0" + tdate;
+}
+var maxDate = (year-18) + "-" + month + "-" + tdate;
+document.getElementById("dob").setAttribute("max",maxDate);
+console.log(maxDate);
